f187ac70598de64311e5e0f499ce4e8193d0d246
Shafayat <mindfreakicup33@gmail.com>
2020-01-26 01:23:10 +0600

    Post Request

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-const members = require("./Members");
+//const members = require("./Members");
 
 const app = express();
 
@@ -13,16 +13,14 @@ const app = express();
 //init middleware
 //app.use(logger);
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //02.Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//03.Creating a simple rest api
-app.get("/api/members", (req, res) => res.json(members));
-
-//05. GET request
-app.get("/api/members/:id", (req, res) => {
-  res.json(members.filter(member => member.id === parseInt(req.params.id)));
-});
+app.use("/api/members", require("./routes/api/members"));
 
 //Port setup and listening to request
 const PORT = process.env.PORT || 5000;
